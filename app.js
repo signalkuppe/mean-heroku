828f6ffe87a04e2c4b8c4571779472db064b60b5
Matteo Leoni <leoni.matteo@gmail.com>
2015-08-27 12:08:35 +0200

    debug

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ var express = require('express'), // express
 		bodyParser = require('body-parser'), // body parser middleware
 		app = express(), // l'app di express
   	path = require('path'), // il modulo path di node,
-		databaseName = 'arch', // il database dell'app
-		databaseHost = 'localhost', // l'host del db,
 		mongoDev = 'mongodb://@localhost/arch',
 		mongoProd = 'mongodb://heroku_w16xlmt2:8gssbg5gdq0jsfcik56kkdncps@ds035703.mongolab.com:35703/heroku_w16xlmt2',
 		mongoUrl =  process.env.NODE_ENV ? mongoProd : mongoDev;
@@ -15,7 +13,6 @@ var express = require('express'), // express
 		ModelSchema = require('./server/models/model'), // schema d esempio di Mongoose
 		ModelRoutes = require('./server/routes/model'); // le routes del modello di esempio
 
-console.log('mongo url@@@@@@@@@@@@@@@@@@@@@@@@ ->',mongoUrl)
 // Connetti to mongodb
 connect();
 mongoose.connection.on('error', function(err) { // se fallisce segnala
@@ -23,16 +20,18 @@ mongoose.connection.on('error', function(err) { // se fallisce segnala
 });
 mongoose.connection.on('disconnected', connect);
 mongoose.connection.on('open', function () {
-	console.info('connesso al database "'+databaseName+'" sull\'host: '+databaseHost+'');
+	console.info('connesso al database',mongoUrl);
 });
 
 // usa body parser per accettare le POST
 app.use(bodyParser.json())
 // usa questo come percorso per le risorse statiche
-app.use('/', express.static(path.join(__dirname, 'client/html')));
+//app.use('/', express.static(path.join(__dirname, 'client/html')));
 // usa anche questo per beccare le index
-app.use('/', express.static(path.join(__dirname, 'client')));
+//app.use('/', express.static(path.join(__dirname, 'client')));
 
+
+app.use(express.static(__dirname + '/client'));
 
 
 app.post('/rest/model', ModelRoutes.save);
